f324c0175ea929ccedb259757721a9394ad4eeef
Ethan Jolles <erjolles@gmail.com>
2021-12-16 13:35:45 -0500

    Gitignore and configuring

--- src/main/java/com/solvd/SocialNetwork/Main/Main.java
+++ src/main/java/com/solvd/SocialNetwork/Main/Main.java
@@ -1,7 +1,11 @@
 package com.solvd.SocialNetwork.Main;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, world");
+        final Logger LOGGER = LogManager.getLogger(Main.class);
+        LOGGER.info("Hello, world");
     }
 }
