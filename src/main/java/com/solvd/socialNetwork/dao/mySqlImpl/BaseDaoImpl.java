2621e64e842d1a4ba0b044c21f16bcf02262febc
Ethan Jolles <erjolles@gmail.com>
2021-12-18 23:04:43 -0500

    Added Hibernate

---
+++ src/main/java/com/solvd/socialNetwork/dao/mySqlImpl/BaseDaoImpl.java
@@ -0,0 +1,25 @@
+package com.solvd.socialNetwork.dao.mySqlImpl;
+
+import com.solvd.socialNetwork.dao.IBaseDao;
+
+public class BaseDaoImpl<T> implements IBaseDao<T> {
+    @Override
+    public void create(T t) {
+
+    }
+
+    @Override
+    public T getById(Integer id) {
+        return null;
+    }
+
+    @Override
+    public void update(T t) {
+
+    }
+
+    @Override
+    public void delete(Integer id) {
+
+    }
+}
