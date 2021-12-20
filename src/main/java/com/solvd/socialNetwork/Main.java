3f61d16b6b60d3feaffc40fac34cd9a5fb9291f9
Ethan Jolles <erjolles@gmail.com>
2021-12-20 13:37:32 -0500

    Done with ConnectionPool? Probably not we'll see

--- src/main/java/com/solvd/socialNetwork/Main.java
+++ src/main/java/com/solvd/socialNetwork/Main.java
@@ -1,41 +1,25 @@
 package com.solvd.socialNetwork;
 
-import com.mysql.cj.jdbc.MysqlConnectionPoolDataSource;
-import com.solvd.socialNetwork.utils.CredentialValues;
+import com.solvd.socialNetwork.utils.ConnectionPool;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import javax.sql.PooledConnection;
 import java.io.IOException;
 import java.sql.*;
 
 public class Main {
 
     final static Logger LOGGER = LogManager.getLogger(Main.class);
+
     public static void main(String[] args) throws IOException, SQLException {
 
-        CredentialValues credentialValues = new CredentialValues();
-        credentialValues.getPropValues("db.properties");
+        ConnectionPool connPool = new ConnectionPool("db.properties");
 
-        MysqlConnectionPoolDataSource ds = new MysqlConnectionPoolDataSource();
-
-        ds.setURL(credentialValues.getUrl());
-        ds.setUser(credentialValues.getUser());
-        ds.setPassword(credentialValues.getPassword());
-
-        PooledConnection pc = ds.getPooledConnection();
-
-        Connection con = pc.getConnection();
-
-        Statement st = con.createStatement();
-
-        ResultSet rs = st.executeQuery("SELECT * FROM SocialNetwork.user");
+        ResultSet rs = connPool.easyQuery("SELECT * FROM SocialNetwork.user");
 
         while (rs.next()) {
-            String data = rs.getString("username");
-            LOGGER.info("Fetching data by column name for row " + rs.getRow() + " : " + data);
-            String dataId = rs.getString("id");
-            LOGGER.info("Fetching data by column name for row " + rs.getRow() + " : " + dataId);
+            LOGGER.info("UserID: " + rs.getString("id"));
+            LOGGER.info("Username: " + rs.getString("username"));
         }
     }
 }
---
+++ src/main/java/com/solvd/socialNetwork/utils/ConnectionPool.java
@@ -0,0 +1,52 @@
+package com.solvd.socialNetwork.utils;
+
+import com.mysql.cj.jdbc.MysqlConnectionPoolDataSource;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
+
+public class ConnectionPool {
+    final private static Logger LOGGER = LogManager.getLogger(ConnectionPool.class);
+    private static final MysqlConnectionPoolDataSource ds = new MysqlConnectionPoolDataSource();
+    private final Properties prop = new Properties();
+
+    public ConnectionPool(String file) throws IOException {
+        getPropValues(file);
+    }
+
+    private void getPropValues(String file) throws IOException {
+        try {
+            InputStream inputStream = getClass().getClassLoader().getResourceAsStream(file);
+
+            if (inputStream != null) {
+                prop.load(inputStream);
+            } else {
+                throw new FileNotFoundException(file + " not found");
+            }
+
+            ds.setURL(prop.getProperty("url"));
+            ds.setUser(prop.getProperty("user"));
+            ds.setPassword(prop.getProperty("password"));
+        } catch (Exception e) {
+            LOGGER.error("Met exception " + e);
+        }
+    }
+
+    public Connection getConnection() throws SQLException {
+        return ds.getConnection();
+    }
+
+    public ResultSet easyQuery(String sql) throws SQLException {
+        Connection connection = getConnection();
+        Statement statement = connection.createStatement();
+        return  statement.executeQuery(sql);
+    }
+}
