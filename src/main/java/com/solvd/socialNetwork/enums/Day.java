28a3d10b105fa7faf7f97ecb300ba7755a09ece1
Ethan Jolles <erjolles@gmail.com>
2021-12-28 17:06:24 -0500

    Added five enums

---
+++ src/main/java/com/solvd/socialNetwork/enums/Day.java
@@ -0,0 +1,8 @@
+package com.solvd.socialNetwork.enums;
+
+public enum Day {
+    MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY;
+
+    Day() {
+    }
+}
---
+++ src/main/java/com/solvd/socialNetwork/enums/ProfileType.java
@@ -0,0 +1,10 @@
+package com.solvd.socialNetwork.enums;
+
+import com.solvd.socialNetwork.model.profile.Profile;
+
+public enum ProfileType {
+    PUBLIC, PRIVATE;
+
+    ProfileType() {
+    }
+}
