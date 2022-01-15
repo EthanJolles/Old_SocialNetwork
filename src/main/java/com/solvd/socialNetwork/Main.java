39642561ba3a3ea3ecb0e7aaf1e7be1bee638240
Ethan Jolles <erjolles@gmail.com>
2022-01-14 19:52:47 -0500

    Update Main.java
    Just changing test cases and making sure ConnectionPool still works

--- src/main/java/com/solvd/socialNetwork/Main.java
+++ src/main/java/com/solvd/socialNetwork/Main.java
@@ -17,40 +17,40 @@ public class Main {
     final static Logger LOGGER = LogManager.getLogger(Main.class);
 
     public static void main(String[] args) {
-        for (int i = 1; i <= 100; i++) {
+        for (int i = 21; i <= 30; i++) {
             try {
                 UserDaoImpl userDao = new UserDaoImpl();
                 User test = new User("test" + i,"" + i);
                 LOGGER.info("Entry:" + i);
                 userDao.create(test);
-            } catch (SQLException e) {
+            } catch (Exception e) {
                 LOGGER.error(e);
             }
         }
 
-        //JaxB unmarshaller
-        try {
-            LOGGER.info(JaxBHandler.unmarshal("product.xml", Product.class));
-        } catch (JAXBException e) {
-            LOGGER.error(e);
-        }
-
-        try {
-            User userObj = new User("Test2", "Test2");
-            LOGGER.info("Attempting to marshal");
-            JaxBHandler.marshal(userObj, User.class, "userObj");
-            LOGGER.info("Marshal completed");
-        } catch (JAXBException e) {
-            LOGGER.error(e);
-        }
-
-        try {
-            ObjectMapper om = new ObjectMapper();
-            User u = om.readValue("src/main/resources/user.json", User.class);
-            String result = om.writeValueAsString(u);
-        }catch(JsonProcessingException e) {
-            LOGGER.error(e);
-        }
+//        //JaxB unmarshaller
+//        try {
+//            LOGGER.info(JaxBHandler.unmarshal("product.xml", Product.class));
+//        } catch (JAXBException e) {
+//            LOGGER.error(e);
+//        }
+//
+//        try {
+//            User userObj = new User("Test2", "Test2");
+//            LOGGER.info("Attempting to marshal");
+//            JaxBHandler.marshal(userObj, User.class, "userObj");
+//            LOGGER.info("Marshal completed");
+//        } catch (JAXBException e) {
+//            LOGGER.error(e);
+//        }
+//
+//        try {
+//            ObjectMapper om = new ObjectMapper();
+//            User u = om.readValue("src/main/resources/user.json", User.class);
+//            String result = om.writeValueAsString(u);
+//        }catch(JsonProcessingException e) {
+//            LOGGER.error(e);
+//        }
     }
 
 
