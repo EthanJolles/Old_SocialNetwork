f971055b118965f0e70118d1b86086bbe3aecf8d
Ethan Jolles <erjolles@gmail.com>
2022-01-16 18:41:19 -0500

    Update Main.java

--- src/main/java/com/solvd/socialNetwork/Main.java
+++ src/main/java/com/solvd/socialNetwork/Main.java
@@ -21,54 +21,54 @@ public class Main {
     final static Logger LOGGER = LogManager.getLogger(Main.class);
 
     public static void main(String[] args) {
-//        // deserializer test
-//        try {
-//            User test = Jackson.deserialize(new File("src/main/resources/jsonObjects/user.json"),User.class);
-//            LOGGER.info(test.getUsername());
-//            LOGGER.info(test.getPassword());
-//        } catch (Exception e) {
-//            LOGGER.error(e);
-//        }
-//
-//        //serializer test
-//        try {
-//            User test = new User("ethanjolles", "solvdIntern123");
-//            Jackson.serialize("test", test);
-//        } catch (Exception e) {
-//            LOGGER.error(e);
-//        }
+        // deserializer test
+        try {
+            User test = Jackson.deserialize(new File("src/main/resources/jsonObjects/user.json"),User.class);
+            LOGGER.info(test.getUsername());
+            LOGGER.info(test.getPassword());
+        } catch (Exception e) {
+            LOGGER.error(e);
+        }
 
-        //database query test for 'i' amount of queries
-//        for (int i = 1; i <= 1; i++) {
-//            try {
-//                UserDaoImpl userDao = new UserDaoImpl();
-//                User test = new User("test" + i,"" + i);
-//                userDao.create(test);
-//                LOGGER.info("Entry:" + i + " completed");
-//            } catch (Exception e) {
-//                LOGGER.error(e);
-//            }
-//        }
-//
-//        //JaxB unmarshaller
-//        try {
-//            LOGGER.info(JaxBHandler.unmarshal("product.xml", Product.class));
-//        } catch (JAXBException e) {
-//            LOGGER.error(e);
-//        }
-//
-//        try {
-//            User userObj = new User("Test2", "Test2");
-//            LOGGER.info("Attempting to marshal");
-//            JaxBHandler.marshal(userObj, User.class, "userObj");
-//            LOGGER.info("Marshal completed");
-//        } catch (JAXBException e) {
-//            LOGGER.error(e);
-//        }
-//        Deadlock multi = new Deadlock();
-//
-////        Deadlock
-//        multi.t1.start();
-//        multi.t2.start();
+        //serializer test
+        try {
+            User test = new User("ethanjolles", "solvdIntern123");
+            Jackson.serialize("test", test);
+        } catch (Exception e) {
+            LOGGER.error(e);
+        }
+
+       //database query test for 'i' amount of queries
+        for (int i = 1; i <= 1; i++) {
+            try {
+                UserDaoImpl userDao = new UserDaoImpl();
+                User test = new User("test" + i,"" + i);
+                userDao.create(test);
+                LOGGER.info("Entry:" + i + " completed");
+            } catch (Exception e) {
+                LOGGER.error(e);
+            }
+        }
+
+        //JaxB unmarshaller
+        try {
+            LOGGER.info(JaxBHandler.unmarshal("product.xml", Product.class));
+        } catch (JAXBException e) {
+            LOGGER.error(e);
+        }
+
+        try {
+            User userObj = new User("Test2", "Test2");
+            LOGGER.info("Attempting to marshal");
+            JaxBHandler.marshal(userObj, User.class, "userObj");
+            LOGGER.info("Marshal completed");
+        } catch (JAXBException e) {
+            LOGGER.error(e);
+        }
+        Deadlock multi = new Deadlock();
+
+//        Deadlock
+        multi.t1.start();
+        multi.t2.start();
     }
 }
